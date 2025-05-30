78aa39ba075d9353476f92a9d7fde38a1d9f9c9d
gusthavo.vieira <gusthavo.vieira@flai.tec.br>
2025-05-30 14:58:02 -0300

    add max connections and opened connections in `/status` endpoint

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,11 +1,33 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("select 1 + 1 as sum;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows);
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  return response.status(200).send("Tudo certo");
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*) as opened_connections FROM pg_stat_activity WHERE datname = 'local_db'",
+  );
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].opened_connections;
+
+  return response.status(200).json({
+    updatedAt: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: parseInt(databaseOpenedConnectionsValue),
+      },
+    },
+  });
 }
 
 export default status;
