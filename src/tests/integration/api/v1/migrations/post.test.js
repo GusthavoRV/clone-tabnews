27477ae6ea29e4514bfab876c3a3f8996d4008a7
gusthavo <gusthavorangel@gmail.com>
2025-06-02 09:11:40 -0300

    add `ESM` in `jest.config.js` file

---
+++ src/tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,14 @@
+import database from "infra/database";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
+}
+
+test("POST to api/vi/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response.status).toBe(200);
+});
